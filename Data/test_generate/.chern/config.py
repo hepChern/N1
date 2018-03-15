cc747af171db633ec5c489b3db91a943d7e1fc67
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-03-15 15:34:16 +0800

    Impress: 6fd88ef5c9e84595be2e288626f9d99c

--- Data/test_generate/.chern/config.py
+++ Data/test_generate/.chern/config.py
@@ -1,1 +1,5 @@
-object_type = "data"
+object_type=u'data'
+pred_impression=['15c7d80fa2e7491db0d59ae738a834cb']
+predecessors=['Generation/test']
+successors=['Selection/test']
+impression=u'6fd88ef5c9e84595be2e288626f9d99c'
