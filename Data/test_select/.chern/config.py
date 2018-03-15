10f530c47a3c3d25dcff7cdf25d4e0439bbbc311
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-03-15 15:34:17 +0800

    Impress: 49ed28783adc4e028772d93dcecdd8ca

--- Data/test_select/.chern/config.py
+++ Data/test_select/.chern/config.py
@@ -1,3 +1,4 @@
+impression=u'49ed28783adc4e028772d93dcecdd8ca'
 object_type=u'data'
-pred_impression=[]
-impression=u'aed6ac361d004e3bb120be04bb458b0e'
+pred_impression=['97c6d0a56fe74942afcc503ac1c9a451']
+predecessors=['Selection/test']
