9116089657b50e79ef9a9f3170838c6d2980e587
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-03-15 15:34:17 +0800

    Impress: 97c6d0a56fe74942afcc503ac1c9a451

--- Selection/test/.chern/config.py
+++ Selection/test/.chern/config.py
@@ -1,2 +1,7 @@
+alias_to_path={'input': 'Data/test_generate', 'output': 'Data/test_select'}
 object_type=u'task'
-predecessors=['Algorithm/Selection']
+path_to_alias={'Data/test_generate': 'input', 'Data/test_select': 'output'}
+pred_impression=['6fd88ef5c9e84595be2e288626f9d99c', '7012d91830474b328ce202963782a3a8']
+predecessors=['Algorithm/Selection', 'Data/test_generate']
+successors=['Data/test_select']
+impression=u'97c6d0a56fe74942afcc503ac1c9a451'
