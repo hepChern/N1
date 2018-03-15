220700b74d052a0be1e4c885f664d99848b4a8d2
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-03-15 15:34:16 +0800

    Impress: 15c7d80fa2e7491db0d59ae738a834cb

--- Generation/test/.chern/config.py
+++ Generation/test/.chern/config.py
@@ -1,2 +1,7 @@
+alias_to_path={'output': 'Data/test_generate'}
 object_type=u'task'
+path_to_alias={'Data/test_generate': 'output'}
+pred_impression=['8f6bce57790744858c43d9800e859f1d']
 predecessors=['Algorithm/Generation']
+successors=['Data/test_generate']
+impression=u'15c7d80fa2e7491db0d59ae738a834cb'
